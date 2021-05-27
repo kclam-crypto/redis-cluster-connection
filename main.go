f94b7ac2627d6bfde5c2b9ebe34049169ba2e543
kc.lam@crypto.com <kc.lam@crypto.com>
2021-05-27 15:50:32 +0800

    add mget

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"encoding/base64"
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
@@ -11,6 +13,11 @@ import (
 )
 
 func main() {
+
+	max := 10
+	prefix := "test:"
+	rand.Seed(time.Now().UnixNano())
+
 	ctx := context.Background()
 	url := os.Args[1]
 	username := os.Args[2]
@@ -27,6 +34,37 @@ func main() {
 		},
 	})
 	defer rdb.Close()
+
+	fmt.Println("---")
+
+	var keys []string
+	for i := 0; i < max; i++ {
+		s := fmt.Sprintf("%024d",rand.Uint64())
+		se := base64.StdEncoding.EncodeToString([]byte(s))
+		key := prefix + se
+		fmt.Printf("set %s, %s\n", key, now)
+		err := rdb.Set(ctx, key, now, 0).Err()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		keys = append(keys,key)
+	}
+
+	fmt.Println("---")
+	for i:=0; i < max; i++ {
+		r := rdb.Get(ctx, keys[i])
+		if r.Err()!= nil {
+			fmt.Println(r.Err())
+		}
+		fmt.Printf("get %s, %s\n", keys[i], r.Val())
+	}
+
+	fmt.Println("---")
+	v := rdb.MGet(ctx, keys...)
+	if v.Err() != nil {
+		fmt.Println(v.Err())
+	}
+	fmt.Printf("%+v\n",v.Val()...)
 
 	fmt.Println("---")
 	fmt.Printf("SET a: %s\n", now)
